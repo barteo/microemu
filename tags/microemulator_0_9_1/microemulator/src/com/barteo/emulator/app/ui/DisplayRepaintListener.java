938a4b58a664d0b2bd9ca6ec934cbd134283b0f4
nobody <nobody@272dc960-a678-11de-8dbb-b5444cbab91d>
2005-09-24 11:52:20 +0000

    This commit was manufactured by cvs2svn to create tag 'microemulator_0_9_1'.

---
+++ tags/microemulator_0_9_1/microemulator/src/com/barteo/emulator/app/ui/DisplayRepaintListener.java
@@ -0,0 +1,11 @@
+package com.barteo.emulator.app.ui;
+
+import com.barteo.emulator.device.MutableImage;
+
+
+public interface DisplayRepaintListener 
+{
+	
+	public void repaintInvoked(MutableImage image);
+	
+}
